73a57dbf8ff6f26692c17601ebccafd340b0b527
bradleypj@icloud.com <Brad@Bradleys-MacBook-Pro.local>
2014-06-26 14:12:15 -0700

    Stubbed out network and auth controller

---
+++ SwiftStackOverFlow/NetworkController.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkController.swift
+//  SwiftStackOverFlow
+//
+//  Created by Bradley Johnson on 6/26/14.
+//  Copyright (c) 2014 Bradley Johnson. All rights reserved.
+//
+
+import Foundation
+
+
+class NetworkController {
+    
+}
